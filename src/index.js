e7b7641585b3b16e93ae68b297f3b660ad9c9a48
BrianJenney <brianjenney83@gmail.com>
2021-01-28 20:16:49 -0800

    log the env var

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
+console.log(process.env, 'ENVIRONMENT');
 axios.defaults.baseURL =
   process.env.REACT_APP_ENV === 'production'
     ? 'https://micasa-services.com'
